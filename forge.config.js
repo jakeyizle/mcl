827b9c78aa98e4299ead995c2bbf7dae41c2f0f8
jakeyizle <whiterbread@gmail.com>
2022-01-23 12:41:42 -0500

    publish fix?

--- forge.config.js
+++ forge.config.js
@@ -27,6 +27,9 @@ module.exports = {
     publishers: [
         {
             name: "@electron-forge/publisher-github",
+            platforms: [
+                "win32"
+              ],
             config: {
                 repository: {
                     owner: "jakeyizle",
