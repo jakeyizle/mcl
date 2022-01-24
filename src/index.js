be1aad9fa976876e54019eee2806a9f6f3bccf42
jakeyizle <whiterbread@gmail.com>
2022-01-23 19:33:45 -0500

    try to fix exe

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const path = require('path');
 const fs = require('fs');
 const os = require('os');
 const db = require('better-sqlite3')('melee.db');
-
+require ('hazardous');
 db.pragma('journal_mode = WAL');
 // db.pragma('analysis_limit=400');
 db.pragma('optimize');
@@ -203,10 +203,10 @@ function initDB() {
     FOREIGN KEY (conversionId) REFERENCES conversions(id)
   )`).run();
 
-  db.prepare('CREATE INDEX IF NOT EXISTS search_index_2 ON conversions (attackingPlayer, attackingCharacter, defendingPlayer, defendingCharacter, stage, percent, moveCount, didKill)').run();
-  db.prepare('CREATE INDEX IF NOT EXISTS count_index ON conversions (id)').run();
-  db.prepare('CREATE INDEX IF NOT EXISTS attacking_index ON conversions (attackingPlayer)').run();
-  db.prepare('CREATE INDEX IF NOT EXISTS defending_index ON conversions (defendingPlayer)').run();
+  // db.prepare('CREATE INDEX IF NOT EXISTS search_index_2 ON conversions (attackingPlayer, attackingCharacter, defendingPlayer, defendingCharacter, stage, percent, moveCount, didKill)').run();
+  // db.prepare('CREATE INDEX IF NOT EXISTS count_index ON conversions (id)').run();
+  // db.prepare('CREATE INDEX IF NOT EXISTS attacking_index ON conversions (attackingPlayer)').run();
+  // db.prepare('CREATE INDEX IF NOT EXISTS defending_index ON conversions (defendingPlayer)').run();
 }
 
 //get all files in all subdirectories
